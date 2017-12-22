857068fd262d5974e5e53c0835297bb6f9305d22
Sean Spradlin <seanspradlin@gmail.com>
2017-12-22 11:44:50 -0600

    wrote test for session

---
+++ test/integration/test.session.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const server = require('../../lib/server');
+const request = require('request-promise').defaults({
+  baseUrl: 'http://localhost:8080/api/',
+  json: true,
+  resolveWithFullResponse: true,
+  simple: false,
+});
+
+describe('/session', () => {
+  before(done => server.listen(8080, () => done()));
+  after(() => server.close());
+
+  it('must return a valid UUID', async (done) => {
+    try {
+      const response = await request.post('session');
+      assert.equal(200, response.statusCode);
+
+      const { session } = response.body;
+      const uuidTest = /([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}){1}/i;
+
+      assert.isTrue(uuidTest.test(session));
+    } catch (error) {
+      done(error);
+    }
+  });
+});

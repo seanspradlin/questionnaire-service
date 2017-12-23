d78a8406024d28366b0a9db1427834fbca162ddf
Sean Spradlin <seanspradlin@gmail.com>
2017-12-22 22:54:37 -0600

    filled out tests for models

--- test/unit/test.models.question.js
+++ test/unit/test.models.question.js
@@ -5,50 +5,102 @@ const { Question } = require('../../lib/models');
 
 describe('question', () => {
   it('should be a function', () => {
-    // Given
-    // When
-    // Then
+    assert.isFunction(Question);
   });
 
   it('should have a question', () => {
     // Given
+    const values = {
+      question: 'What is 2 + 2?',
+      answers: ['1', '2', '3'],
+    };
+
     // When
+    const question = new Question(values);
+
     // Then
+    assert.isString(question.question);
   });
 
   it('should have answers', () => {
     // Given
+    const values = {
+      question: 'What is 2 + 1?',
+      answers: ['1', '2', '3'],
+    };
+
     // When
+    const question = new Question(values);
+
     // Then
+    assert.isArray(question.answers);
   });
 
   it('should throw error if question is not provided', () => {
     // Given
+    const values = {
+      answers: ['1', '2', '3'],
+    };
+
     // When
+    const instantiate = () => new Question(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if question is not a string', () => {
     // Given
+    const values = {
+      question: 42,
+      answers: ['1', '2', '3'],
+    };
+
     // When
+    const instantiate = () => new Question(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if no answers are provided', () => {
     // Given
+    const values = {
+      question: 'What is 2 + 1?',
+    };
+
     // When
+    const instantiate = () => new Question(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if answers are not an array of strings', () => {
     // Given
+    const values = {
+      question: 'What is 2 + 1?',
+      answers: [1, 2, 3],
+    };
+
     // When
+    const instantiate = () => new Question(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if there are less than two answers', () => {
     // Given
+    const values = {
+      question: 'What is 2 + 2?',
+      answers: ['3'],
+    };
+
     // When
+    const question = new Question(values);
+
     // Then
+    assert.isString(question.question);
   });
 });
--- test/unit/test.models.session.js
+++ test/unit/test.models.session.js
@@ -1,54 +1,93 @@
 process.env.NODE_ENV = 'test';
 
 const { assert } = require('chai');
+const uuid = require('uuid/v4');
 const { Session } = require('../../lib/models');
 
 describe('session', () => {
   it('should be a function', () => {
-    // Given
-    // When
-    // Then
+    assert.isFunction(Session);
   });
 
   it('should have an id', () => {
     // Given
+    const values = {
+      id: uuid(),
+      questionsAsked: [uuid(), uuid()],
+    };
+
     // When
+    const session = new Session(values);
+
     // Then
+    assert.isString(session.id);
   });
 
   it('should have questionsAsked', () => {
     // Given
+    const values = {
+      id: uuid(),
+      questionsAsked: [uuid(), uuid()],
+    };
+
     // When
+    const session = new Session(values);
+
     // Then
+    assert.isArray(session.questionsAsked);
   });
 
   it('should throw error if id is invalid', () => {
     // Given
+    const values = {
+      id: 'invalid-token',
+      questionsAsked: [uuid(), uuid()],
+    };
+
     // When
+    const instantiate = () => new Session(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw error if no id is provided', () => {
     // Given
-    // When
-    // Then
-  });
+    const values = {
+      questionsAsked: [uuid(), uuid()],
+    };
 
-  it('should throw error if id is empty', () => {
-    // Given
     // When
+    const instantiate = () => new Session(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if no questionsAsked provided', () => {
     // Given
+    const values = {
+      id: uuid(),
+    };
+
     // When
+    const instantiate = () => new Session(values);
+
     // Then
+    assert.throws(instantiate);
   });
 
   it('should throw an error if questionsAsked is not an array', () => {
     // Given
+    const values = {
+      id: uuid(),
+      questionsAsked: 'invalid',
+    };
+
     // When
+    const instantiate = () => new Session(values);
+
     // Then
+    assert.throws(instantiate);
   });
 });

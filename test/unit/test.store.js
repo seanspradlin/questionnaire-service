77383d1f2060adb6d8c50e4e07bc87551957398e
Sean Spradlin <seanspradlin@gmail.com>
2017-12-24 09:54:05 -0600

    fixed tests - mock-redis is a singleton, cannot async and done statements

--- test/unit/test.store.js
+++ test/unit/test.store.js
@@ -7,6 +7,9 @@ const Store = require('../../lib/store');
 const { Question, Session } = require('../../lib/models');
 const Promise = require('bluebird');
 
+const client = Promise.promisifyAll(redis.createClient());
+const store = new Store(client);
+
 describe('store', () => {
   describe('constructor', () => {
     it('should require a redis client', () => {
@@ -19,294 +22,191 @@ describe('store', () => {
   });
 
   describe('getSession', () => {
-    it('should get a session', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const session = new Session();
-        const hmset = Promise.promisify(client.hmset);
-        await hmset(session.getRedisKey(), session);
+    it('should get a session', async () => {
+      // Given
+      const session = new Session();
+      await client.hmsetAsync(session.getRedisKey(), session);
 
-        // When
-        const retrieved = await store.getSession(session.id);
+      // When
+      const retrieved = await store.getSession(session.id);
 
-        // Then
-        assert.equal(retrieved.id, session.id);
-        assert.instanceOf(retrieved, Session);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.equal(retrieved.id, session.id);
+      assert.instanceOf(retrieved, Session);
     });
 
-    it('should return null if no session found', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const id = uuid();
+    it('should return null if no session found', async () => {
+      // Given
+      const id = uuid();
 
-        // When
-        const retrieved = await store.getSession(id);
+      // When
+      const retrieved = await store.getSession(id);
 
-        // Then
-        assert.isNull(retrieved);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.isNull(retrieved);
     });
 
-    it('should return an error if no key provided', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-
-        // When
-        await store.getSession();
-
-        // Then
-        assert.fail();
-        done(new Error('should have errored'));
-      } catch (error) {
-        done();
-      }
+    it('should return an error if no key provided', (done) => {
+      // When
+      store
+        .getSession()
+        .then(() => {
+          done(new Error('should have thrown'));
+        })
+        .catch(() => {
+          done();
+        });
     });
   });
 
   describe('saveSession', () => {
-    it('should save a session', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const session = new Session();
+    it('should save a session', async () => {
+      // Given
+      const session = new Session();
 
-        // When
-        await store.saveSession(session);
+      // When
+      await store.saveSession(session);
 
-        // Then
-        const hgetall = Promise.promisify(client.hgetall);
-        const hash = await hgetall(session.getRedisKey());
-        assert.equal(hash.id, session.id);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      const hash = await client.hgetallAsync(session.getRedisKey());
+      assert.equal(hash.id, session.id);
     });
 
-    it('should error if trying to save invalid session', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const session = { invalid: 'not a session' };
+    it('should error if trying to save invalid session', (done) => {
+      // Given
+      const session = { invalid: 'not a session' };
 
-        // When
-        await store.saveSession(session);
-
-        // Then
-        assert.fail();
-        done(new Error('Error should have been caught'));
-      } catch (error) {
-        done();
-      }
+      // When
+      store
+        .saveSession(session)
+        .then(() => {
+          done(new Error('should have thrown error'));
+        })
+        .catch(() => done());
     });
 
-    it('should create an entry in the session keys list', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const session = new Session();
-        const scard = Promise.promisify(client.scard);
+    it('should create an entry in the session keys list', async () => {
+      // Given
+      const session = new Session();
 
-        // When
-        await store.saveSession(session);
+      // When
+      await store.saveSession(session);
 
-        // Then
-        const len = await scard('sessionList');
-        assert.equal(len, 1);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      const members = await client.smembersAsync('sessionKeys');
+      assert.include(members, session.id);
     });
   });
 
   describe('getSessionKeys', () => {
-    it('should get a list of keys', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const sadd = Promise.promisify(client.sadd);
-        const id = uuid();
-        await sadd('sessionKeys', id);
+    it('should get a list of keys', async () => {
+      // Given
+      const id = uuid();
+      await client.saddAsync('sessionKeys', id);
 
-        // When
-        const keys = await store.getSessionKeys();
+      // When
+      const keys = await store.getSessionKeys();
 
-        // Then
-        assert.isArray(keys);
-        assert.equal(keys.length, 1);
-        assert.isString(keys[0]);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.isArray(keys);
+      assert.equal(keys.length, 1);
+      assert.isString(keys[0]);
     });
   });
 
   describe('getQuestion', () => {
-    it('should get a question', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const question = new Question({
-          question: 'What is the best Toto song?',
-          answers: ['Africa', 'Hold the Line'],
-        });
-        const hmset = Promise.promisify(client.hmset);
-        await hmset(question.getRedisKey(), question);
+    it('should get a question', async () => {
+      // Given
+      const question = new Question({
+        question: 'What is the best Toto song?',
+        answers: ['Africa', 'Hold the Line'],
+      });
+      await client.hmsetAsync(question.getRedisKey(), question);
 
-        // When
-        const retrieved = await store.getQuestion(question.id);
+      // When
+      const retrieved = await store.getQuestion(question.id);
 
-        // Then
-        assert.equal(retrieved.id, question.id);
-        assert.instanceOf(retrieved, Question);
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.equal(retrieved.id, question.id);
+      assert.instanceOf(retrieved, Question);
     });
 
-    it('should return null if no question found', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const id = uuid();
+    it('should return null if no question found', async () => {
+      // Given
+      const id = uuid();
 
-        // When
-        const retrieved = await store.getQuestion(id);
+      // When
+      const retrieved = await store.getQuestion(id);
 
-        // Then
-        assert.isNull(retrieved);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.isNull(retrieved);
     });
 
-    it('should return an error if no key provided', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-
-        // When
-        await store.getQuestion();
-
-        // Then
-        assert.fail();
-        done(new Error('should have errored'));
-      } catch (error) {
-        done();
-      }
+    it('should return an error if no key provided', (done) => {
+      // When
+      store
+        .getQuestion()
+        .then(() => done(new Error('should have thrown error')))
+        .catch(() => done());
     });
   });
 
   describe('saveQuestion', () => {
-    it('should save a question', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const question = new Question({
-          question: 'What is the best Toto song?',
-          answers: ['Africa', 'Hold the Line'],
-        });
+    it('should save a question', async () => {
+      // Given
+      const question = new Question({
+        question: 'What is the best Toto song?',
+        answers: ['Africa', 'Hold the Line'],
+      });
 
-        // When
-        await store.saveQuestion(question);
+      // When
+      await store.saveQuestion(question);
 
-        // Then
-        const hgetall = Promise.promisify(client.hgetall);
-        const hash = await hgetall(question.getRedisKey());
-        assert.equal(hash.id, question.id);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      const hash = await client.hgetallAsync(question.getRedisKey());
+      assert.equal(hash.id, question.id);
     });
 
-    it('should error if trying to save an invalid question', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const question = { invalid: 'not a question' };
+    it('should error if trying to save an invalid question', async () => {
+      // Given
+      const question = { invalid: 'not a question' };
 
-        // When
-        await store.saveQuestion(question);
+      // When
+      await store.saveQuestion(question);
 
-        // Then
-        assert.fail();
-        done(new Error('Error should have been caught'));
-      } catch (error) {
-        done();
-      }
+      // Then
+      assert.fail();
     });
 
-    it('should create an entry in the question keys list', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const question = new Question({
-          question: 'What is the best Toto song?',
-          answers: ['Africa', 'Hold the Line'],
-        });
-        const scard = Promise.promisify(client.scard);
+    it('should create an entry in the question keys list', async () => {
+      // Given
+      const question = new Question({
+        question: 'What is the best Toto song?',
+        answers: ['Africa', 'Hold the Line'],
+      });
 
-        // When
-        await store.saveQuestion(question);
+      // When
+      await store.saveQuestion(question);
 
-        // Then
-        const len = await scard('questionList');
-        assert.equal(len, 1);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      const members = await client.smembersAsync('questionKeys');
+      assert.include(members, question.id);
     });
   });
 
   describe('getQuestionKeys', () => {
-    it('should get a list of keys', async (done) => {
-      try {
-        // Given
-        const client = redis.createClient();
-        const store = new Store(client);
-        const sadd = Promise.promisify(client.sadd);
-        const id = uuid();
-        await sadd('questionKeys', id);
+    it('should get a list of keys', async () => {
+      // Given
+      const id = uuid();
+      await client.saddAsync('questionKeys', id);
 
-        // When
-        const keys = await store.getQuestionKeys();
+      // When
+      const keys = await store.getQuestionKeys();
 
-        // Then
-        assert.isArray(keys);
-        assert.equal(keys.length, 1);
-        assert.isString(keys[0]);
-        done();
-      } catch (error) {
-        done(error);
-      }
+      // Then
+      assert.isArray(keys);
+      assert.equal(keys.length, 1);
+      assert.isString(keys[0]);
     });
   });
 });

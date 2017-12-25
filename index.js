ea7ad361e78284cb4accfac0aeb71716c75ab0ce
Sean Spradlin <seanspradlin@gmail.com>
2017-12-24 23:09:40 -0600

    added env var for redis client

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Server = require('./lib/server');
 const logger = require('./lib/logger');
 const redis = require('redis');
 
-const client = redis.createClient();
-const server = new Server(client);
-
+const REDIS = process.env.REDIS || '//localhost:6379';
 const PORT = process.env.PORT || 8080;
+
+const client = redis.createClient(REDIS);
+const server = new Server(client);
 
 server.listen(PORT, () => {
   logger.info(`Server running on ${PORT}`);
